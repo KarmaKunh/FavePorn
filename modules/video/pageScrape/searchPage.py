8317ac62a0865cef7a933c9dec37b5bf977f0b36
Karim Chahir <56369066+KarmaKunh@users.noreply.github.com>
2022-07-07 04:24:03 +0200

    Update searchPage.py

--- modules/video/pageScrape/searchPage.py
+++ modules/video/pageScrape/searchPage.py
@@ -289,22 +289,22 @@ def get_url( url):
 
         #}
         headers= {
-                'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
-                'accept-encoding' : 'gzip, deflate, br'
-                'accept-language' : 'it-IT,it;q=0.9'
-                'connection' : 'keep-alive'
-                'host' : 'em.phncdn.com'
-                'if-modified-since' : 'Sat, 04 Jun 2022 21:24:13 GMT'
-                'range' : 'bytes=0-6529755'
-                'sec-ch-ua' : '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"'
-                'sec-ch-ua-mobile' : '?1'
-                'sec-ch-ua-platform' : '"Android"'
-                'sec-fetch-dest' : 'document'
-                'sec-fetch-mode' : 'navigate'
-                'sec-fetch-site' : 'none'
-                'sec-fetch-user' : '?1'
-                'upgrade-insecure-requests' : '1'
-                'user-agent' : 'Mozilla/5.0 (Linux; Android 9; LLD-L31) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36'
+                'accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+                'accept-encoding' : 'gzip, deflate, br',
+                'accept-language' : 'it-IT,it;q=0.9',
+                'connection' : 'keep-alive',
+                'host' : 'em.phncdn.com',
+                'if-modified-since' : 'Sat, 04 Jun 2022 21:24:13 GMT',
+                'range' : 'bytes=0-6529755',
+                'sec-ch-ua' : '" Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+                'sec-ch-ua-mobile' : '?1',
+                'sec-ch-ua-platform' : '"Android"',
+                'sec-fetch-dest' : 'document',
+                'sec-fetch-mode' : 'navigate',
+                'sec-fetch-site' : 'none',
+                'sec-fetch-user' : '?1',
+                'upgrade-insecure-requests' : '1',
+                'user-agent' : 'Mozilla/5.0 (Linux; Android 9; LLD-L31) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.79 Mobile Safari/537.36',
         }
 
         # req = urllib.request.Request( url)
